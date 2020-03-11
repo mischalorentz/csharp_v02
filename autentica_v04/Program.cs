0698660a97a80a6dc7891add557b25e984c31f82
mischalorentz <mlorentz.dca@gmail.com>
2020-03-11 20:49:14 -0300

    first commit

---
+++ autentica_v04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace autentica_v04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
